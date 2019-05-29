6f229f33eb27f889305e5144265d64903d5ef6cc
Carlos Fabela <cfabela@itexico.com>
2019-05-28 21:47:52 -0500

    Adding first mainPage to the project

---
+++ TripLog/TripLog/Models/TripLogEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TripLog.Models
+{
+    public class TripLogEntry
+    {
+        public string Title { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public DateTime Date { get; set; }
+        public int Rating { get; set; }
+        public string Notes { get; set; }
+    }
+}
