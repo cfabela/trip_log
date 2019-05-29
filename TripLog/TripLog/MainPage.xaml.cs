fb549b077d66899e7ed08f3213fa34b6ec96175f
Carlos Fabela <cfabela@itexico.com>
2019-05-28 21:58:48 -0500

    Adding main Screen

--- TripLog/TripLog/MainPage.xaml.cs
+++ TripLog/TripLog/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TripLog.Models;
 using Xamarin.Forms;
 
 namespace TripLog
@@ -13,9 +14,46 @@ namespace TripLog
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
+            trips.ItemsSource = GetItems();
+            
+        }
+
+        private List<TripLogEntry> GetItems()
+        {
+            return new List<TripLogEntry>
+            {
+                new TripLogEntry
+                {
+                    Title = "Washington Monument",
+                    Notes = "Amazing!",
+                    Rating = 3,
+                    Date = new DateTime(2017,2,5),
+                    Latitude = 38.8895,
+                    Longitude = -77.0352
+                },
+                 new TripLogEntry
+                {
+                    Title = "Statue of Liberty",
+                    Notes = "Inspiring!",
+                    Rating = 4,
+                    Date = new DateTime(2017,4,13),
+                    Latitude = 40.6892,
+                    Longitude = -74.0444
+                },
+                  new TripLogEntry
+                {
+                    Title = "Golden Gate Bridge",
+                    Notes = "Foggy but beautiful.",
+                    Rating = 5,
+                    Date = new DateTime(2017,4,26),
+                    Latitude = 37.8268,
+                    Longitude = -122.4798
+                }
+            };
         }
     }
 }
--- TripLog/TripLog/Models/TripLogEntry.cs
+++ TripLog/TripLog/Models/TripLogEntry.cs
@@ -4,8 +4,8 @@ namespace TripLog.Models
     public class TripLogEntry
     {
         public string Title { get; set; }
-        public string Latitude { get; set; }
-        public string Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
         public DateTime Date { get; set; }
         public int Rating { get; set; }
         public string Notes { get; set; }
