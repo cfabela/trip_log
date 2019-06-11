49de41924a045cc73db61a8ae7c39ff39a613548
Carlos Fabela <cfabela@itexico.com>
2019-06-10 22:32:07 -0500

    Adding pull to refresh simulation

--- TripLog/TripLog/ViewModels/MainViewModel.cs
+++ TripLog/TripLog/ViewModels/MainViewModel.cs
@@ -45,47 +45,62 @@ namespace TripLog.ViewModels
             }
         }
 
-        public override async Task Init()
+        private Command _refreshCommand;
+        public Command Refreshcommand
         {
-            LoadEntries();
-        }
-
-        private void LoadEntries()
-        {
-            LogEntries.Clear();
-
-            //   await Task.Factory.StartNew(() =>
+            get
             {
-                LogEntries.Add(new TripLogEntry
-                {
-                    Title = "Washington Monument",
-                    Notes = "Amazing!",
-                    Rating = 3,
-                    Date = new DateTime(2017, 2, 5),
-                    Latitude = 38.8895,
-                    Longitude = -77.0352
-                });
-                LogEntries.Add(new TripLogEntry
-                {
-                    Title = "Statue of Liberty",
-                    Notes = "Inspiring!",
-                    Rating = 4,
-                    Date = new DateTime(2017, 4, 13),
-                    Latitude = 40.6892,
-                    Longitude = -74.0444
-                });
-                LogEntries.Add(new TripLogEntry
-                {
-                    Title = "Golden Gate Bridge",
-                    Notes = "Foggy but beautiful.",
-                    Rating = 5,
-                    Date = new DateTime(2017, 4, 26),
-                    Latitude = 37.8268,
-                    Longitude = -122.4798
-                });
-                //   });
+                return _refreshCommand
+                    ?? (_refreshCommand = new Command(async () => await LoadEntries()));
             }
         }
+
+        public override async Task Init()
+        {
+           await LoadEntries();
+        }
+
+        private async Task LoadEntries()
+        {
+            if (IsBusy) return;
+            IsBusy = true;
+
+            LogEntries.Clear();
+            await Task.Delay(3000);
+
+          //  await Task.Factory.StartNew(() =>
+           //{
+               LogEntries.Add(new TripLogEntry
+               {
+                   Title = "Washington Monument",
+                   Notes = "Amazing!",
+                   Rating = 3,
+                   Date = new DateTime(2017, 2, 5),
+                   Latitude = 38.8895,
+                   Longitude = -77.0352
+               });
+               LogEntries.Add(new TripLogEntry
+               {
+                   Title = "Statue of Liberty",
+                   Notes = "Inspiring!",
+                   Rating = 4,
+                   Date = new DateTime(2017, 4, 13),
+                   Latitude = 40.6892,
+                   Longitude = -74.0444
+               });
+               LogEntries.Add(new TripLogEntry
+               {
+                   Title = "Golden Gate Bridge",
+                   Notes = "Foggy but beautiful.",
+                   Rating = 5,
+                   Date = new DateTime(2017, 4, 26),
+                   Latitude = 37.8268,
+                   Longitude = -122.4798
+               });
+            // });
+            IsBusy = false;
+        }
+    
 
         private async Task ExecuteViewCommand(TripLogEntry entry)
         {
